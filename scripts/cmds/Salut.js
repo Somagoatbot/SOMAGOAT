ad47b7ea712d4fa1b29976b62dcc549ad09ac707
Somagoatbot <somagoatbot@gmail.com>
2024-06-28 14:29:49 +0200

    Update Salut.js

--- scripts/cmds/Salut.js
+++ scripts/cmds/Salut.js
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "salut") return message.reply("𝘴𝘢𝘭𝘶𝘵 𝘮𝘰𝘯 𝘱𝘰𝘵𝘦 𝘮𝘰𝘪 𝘤'𝘦𝘴𝘵 𝘵𝘰𝘮𝘰𝘶𝘳𝘢 𝘫𝘶𝘯𝘪𝘰𝘳 𝘱𝘰𝘴𝘦 𝘮𝘰𝘪 𝘵𝘦𝘴 𝘲𝘶𝘦𝘴𝘵𝘪𝘰𝘯 🤲🤍");
+    if (event.body && event.body.toLowerCase() == "salut") return message.reply("YO COMMENT TU VAS SUIS LA POUR TE SERVIR");
 }
 };
